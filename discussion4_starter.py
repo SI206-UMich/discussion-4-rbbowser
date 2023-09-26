9e712632abee9846b98bbc70299590876768b348
rbbowser <145046590+rbbowser@users.noreply.github.com>
2023-09-26 13:20:49 -0400

    Add files via upload

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -7,35 +7,47 @@ class Rectangle():
 
     # YOUR CODE HERE
 
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
     # Create the "__str__" method
-    #
     # It returns a string, 
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
 
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
 
     # Create the "verify_input" method
-    #
-    # It returns a boolean
     #       True if the width and height are positive numbers
     #       False otherwise
 
     # YOUR CODE HERE
 
+    def verify_input(self):
+        if ((self.width > 0) and (self.height > 0)):
+            return True
+        else:
+            return False
 
 
     # Create the "area" method
-    #
     # It first verifies inputs and return "Invalid input" if they are invalid.
     # Otherwise, it returns the area of the rectangle.
 
     # YOUR CODE HERE
 
+    def area(self):
+        if self.verify_input():
+            self.area = self.width * self.height
+            return self.area
 
+        else:
+            return "Invalid input"
 
     # Create the "perimeter" method
     #
@@ -45,19 +57,13 @@ class Rectangle():
     # YOUR CODE HERE
     
 
+    def perimeter(self):
+        if self.verify_input():
+            self.perimeter = 2 * (self.width + self.height)
+            return self.perimeter
+        
+        else:
+            return "Invalid input"
 
 def main():
     r = Rectangle(10, 10)
-    print(r)
-    print("Area:", r.area())
-    print("Perimeter:", r.perimeter())
-    print()
-
-    r = Rectangle(0, 10)
-    print(r)
-    print("Area:", r.area())
-    print("Perimeter:", r.perimeter())
-    print()
-
-if __name__ == "__main__":
-    main()
